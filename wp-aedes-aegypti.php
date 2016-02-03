714d1d7fefdfa44e4de474c6d9adca0c1b077988
Jacson Passold <jacson@ethymos.com.br>
2016-02-03 14:44:16 -0200

    remove unnecessary files and update urls

--- wp-aedes-aegypti.php
+++ wp-aedes-aegypti.php
@@ -15,6 +15,8 @@ Domain Path: /languages
 
 function wp_aedes_aegypti() {
 	wp_enqueue_script( 'wp_aedes_aegypti', plugin_dir_url(__FILE__) . 'aedes.js', array( 'jquery') );
+	wp_localize_script('wp_aedes_aegypti', 'wp_aedes_aegypti', array( 'pluginUrl' => plugin_dir_url( __FILE__ )));
+	wp_enqueue_style('wp_aedes_aegypti', plugin_dir_url(__FILE__) . 'aedes.css');
 }
 add_action( 'wp_enqueue_scripts', 'wp_aedes_aegypti' );
 
